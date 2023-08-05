d57c0520a222799dc7551c64c2bcfbc31e35f75e
p2pV1 <pplendeverest@gmail.com>
2023-08-05 17:45:00 -0400

    Main pipeline line push

--- backend/projectV1/manage.py
+++ backend/projectV1/manage.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+if 'db' not in os.environ:
+    # Use SQLite for Docker build
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'projectV1.settings_sqlite')
+else:
+    # Use regular settings
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectV1.settings')
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectV1.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django."
         ) from exc
     execute_from_command_line(sys.argv)
 
