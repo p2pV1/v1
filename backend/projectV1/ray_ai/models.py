dca955c5fbd21adf32ae345b4068bef641926093
p2pV1 <pplendeverest@gmail.com>
2023-08-20 14:09:35 -0400

    docker fixes

--- backend/projectV1/ray_ai/models.py
+++ backend/projectV1/ray_ai/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.PositiveIntegerField()
-
-    def get_name(self):
-        return self.name
-
-    def get_age(self):
-        return self.age
-
-
 class BestConfig(models.Model):
     n_estimators = models.PositiveIntegerField()
     accuracy = models.FloatField()
+
+    def __str__(self):
+        return f"n_estimators: {self.n_estimators}, accuracy: {self.accuracy}"
--- backend/projectV1/ray_ai/views.py
+++ backend/projectV1/ray_ai/views.py
@@ -6,8 +6,6 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-from .models import User, BestConfig
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -24,42 +22,19 @@ def train_model(config):
     return model.score(X_test, y_test)
 
 
-def ray_train(request):
+def ray_train_view(request):
     config = {
         "n_estimators": tune.grid_search([10, 25, 50, 75, 100])
     }
     analysis = tune.run(train_model, config=config)
     best_config = analysis.get_best_config(metric="mean_accuracy")
-    # Save to database
-    BestConfig.objects.create(
-        n_estimators=best_config["n_estimators"], accuracy=analysis.get_best_result()["mean_accuracy"])
-    return JsonResponse({"best_config": best_config})
-
-
-def actor_view(request):
-    best_config_obj = BestConfig.objects.last()
-
-    if not best_config_obj:
-        n_estimators_best = 'Not available'
-        accuracy_best = 'Not available'
-    else:
-        n_estimators_best = best_config_obj.n_estimators
-        accuracy_best = best_config_obj.accuracy
-
-    user = User.remote("Alice", 30)
-    ref1 = user.get_name.remote()
-    ref2 = user.get_age.remote()
-
-    name = ray.get(ref1)
-    age = ray.get(ref2)
+    best_accuracy = analysis.get_best_result()["mean_accuracy"]
 
     context = {
-        'name': name,
-        'age': age,
-        'best_n_estimators': n_estimators_best,
-        'best_accuracy': accuracy_best
+        'best_n_estimators': best_config["n_estimators"],
+        'best_accuracy': best_accuracy
     }
-    return render(request, 'ray_ai/actor.html', context)
+    return render(request, 'ray_ai/actors.html', context)
 
 
 def index(request):
