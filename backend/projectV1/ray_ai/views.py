eaa47c6676eaaa85d95c12599776e2583b062b14
p2pV1 <pplendeverest@gmail.com>
2023-08-22 11:06:24 -0400

    Ray and Database Seperated for Serialization

--- backend/projectV1/ray_ai/views.py
+++ backend/projectV1/ray_ai/views.py
@@ -5,42 +5,59 @@ from ray import tune
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
 import logging
 
 logger = logging.getLogger(__name__)
 
+# Note: No Django imports here.
+
 
 def train_model(config):
-    iris = load_iris()
-    X = iris.data
-    y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    try:
+        iris = load_iris()
+        X = iris.data
+        y = iris.target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2)
 
-    model = RandomForestClassifier(n_estimators=config["n_estimators"])
-    model.fit(X_train, y_train)
-    return {"accuracy": model.score(X_test, y_test)}
+        model = RandomForestClassifier(n_estimators=config["n_estimators"])
+        model.fit(X_train, y_train)
+        return {"accuracy": model.score(X_test, y_test)}
+    except Exception as e:
+        logger.error(f"Error during training: {e}")
+        return {"accuracy": 0.0}
 
 
 def ray_train_view(request):
-    # Initialize ray, ignoring if it's already been initialized.
-    ray.init(ignore_reinit_error=True)
+    # Import within the function to avoid issues with Ray.
+    from .models import BestConfig
 
-    config = {
-        "n_estimators": tune.grid_search([10, 25, 50, 75, 100])
-    }
-    analysis = tune.run(train_model, config=config,
-                        metric="accuracy", mode="max")
+    context = {}
+    try:
+        ray.init(ignore_reinit_error=True)
+        config = {
+            "n_estimators": tune.grid_search([10, 25, 50, 75, 100])
+        }
+        analysis = tune.run(train_model, config=config,
+                            metric="accuracy", mode="max")
+        best_trial = analysis.get_best_trial(metric="accuracy", mode="max")
+        best_config = best_trial.config
+        best_accuracy = best_trial.last_result["accuracy"]
 
-    # Fetching the best config and result using the specified metric and mode
-    best_trial = analysis.get_best_trial(metric="accuracy", mode="max")
-    best_config = best_trial.config
-    best_accuracy = best_trial.last_result["accuracy"]
+        # Save to database here, inside Django's context.
+        best_config_db = BestConfig(
+            n_estimators=best_config["n_estimators"], accuracy=best_accuracy)
+        best_config_db.save()
 
-    context = {
-        'best_n_estimators': best_config["n_estimators"],
-        'best_accuracy': best_accuracy
-    }
+        context = {
+            'best_n_estimators': best_config["n_estimators"],
+            'best_accuracy': best_accuracy
+        }
+    except Exception as e:
+        logger.error(f"Error during ray_train_view: {e}")
+        context = {
+            'error': f"An error occurred: {e}"
+        }
     return render(request, 'ray_ai/actors.html', context)
 
 
