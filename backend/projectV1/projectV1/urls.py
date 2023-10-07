3e3ff334abedfb7446ce9f28a85f50700992589a
Ashish Regmi <ashishregmi2017@gmail.com>
2023-10-06 22:45:38 -0400

    Your commit message here

--- backend/projectV1/projectV1/urls.py
+++ backend/projectV1/projectV1/urls.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.urls import path, include
 from django.contrib import admin
+from registration.schema import schema 
+from graphene_django.views import GraphQLView
 from . import views
 
-urlpatterns = [
+# Import necessary modules for serving static files during development
+from django.conf import settings
+from django.conf.urls.static import static
 
-    # Changed index path
-    # path('', views.index, name='home'),
+urlpatterns = [
+    # ... your existing urlpatterns ...
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema), name='graphql'),  
     path('ray_ai/', include('ray_ai.urls')),
     path('registration/', include('registration.urls')),
     path('synthetic_data/', include('synthetic_data.urls')),
@@ -16,6 +21,9 @@ urlpatterns = [
     path('openai/', include('openai_integration.urls')),
 ]
 
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 def index(request):
     return HttpResponse("Welcome to the app!")
