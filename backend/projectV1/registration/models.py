76e0711c14a4d0970323bd6aae00c13407bc433d
pratikdahal105 <pratikdahal105@gmail.com>
2023-10-07 13:45:58 -0400

    updated model

--- backend/projectV1/registration/models.py
+++ backend/projectV1/registration/models.py
@@ -7,16 +7,9 @@ class Profile(models.Model):
     verified_at = models.DateField(null=True, blank=True)
     phone = models.TextField(max_length=20, blank=True)
     sub = models.TextField(max_length=100, blank=True)
-    age = models.PositiveIntegerField(null=True, blank=True)  # New field
+    dob = models.DateField(null=True, blank=True)  # New field
     name = models.CharField(max_length=100, null=True, blank=True)  # New field
 
     class Meta:
         db_table = 'profile'
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
