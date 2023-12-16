1e0f3d418659629ab7cefe1005a70ed35e0a9d90
Ashish Regmi <ashishregmi2017@gmail.com>
2023-12-16 16:55:53 -0500

    Frontend Added

--- frontend/v1-front-end/src/components/chat/messagearea.js
+++ frontend/v1-front-end/src/components/chat/messagearea.js
@@ -8,19 +8,49 @@ const MessageArea = ({ backendUrl, userData }) => {
   const [newMessage, setNewMessage] = useState('');
   const [socket, setSocket] = useState(null);
 
-  const sendMessage = async () => {
-    // Implement the logic for sending a message here
-    // You can use the WebSocket or make an API request to send the message
-    // Update the state or perform the necessary actions
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(JSON.stringify({ message: newMessage, user: userData.id }));
-      setNewMessage('');
+  useEffect(() => {
+    setIsLoading(true);
+    fetchMessages();  // Fetch initial messages
+
+    // Initialize WebSocket connection if not already connected
+    if (!socket) {
+      const protocol = backendUrl.startsWith('https') ? 'wss' : 'ws';
+      const wsUrl = `${protocol}://${"localhost:8080"}/ws/room/${slug}/`;
+      const ws = new WebSocket(wsUrl);
+
+      ws.onopen = () => {
+        console.log('WebSocket connected');
+        setIsLoading(false);
+      };
+
+      ws.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        setMessages((prevMessages) => [...prevMessages, data]);
+      };
+
+      ws.onerror = (error) => {
+        console.error('WebSocket error:', error);
+        setIsLoading(false);
+      };
+
+      ws.onclose = () => {
+        console.log('WebSocket closed');
+        setIsLoading(false);
+        setSocket(null);
+      };
+
+      setSocket(ws);
     }
-    console.log('Sending message:', newMessage);
-  };
+
+    return () => {
+      if (socket) {
+        socket.close();
+        console.log('WebSocket connection closed');
+      }
+    };
+  }, [backendUrl, slug]); // Dependencies for useEffect
 
   const fetchMessages = async () => {
-    setIsLoading(true);
     try {
       const response = await fetch(`${backendUrl}/api/room/rooms/${slug}/messages/`, {
         method: 'GET',
@@ -38,89 +68,48 @@ const MessageArea = ({ backendUrl, userData }) => {
     }
   };
 
-  useEffect(() => {
-    fetchMessages();
-    // const protocol = backendUrl.startsWith('https') ? 'wss' : 'ws';
-    // const wsUrl = `${protocol}://${backendUrl}/ws/room/${slug}/`;
-    // const ws = new WebSocket(wsUrl);
-
-    // ws.onopen = () => {
-    //   console.log('WebSocket connected');
-    //   setIsLoading(false);
-    // };
-
-    // ws.onmessage = (e) => {
-    //   const data = JSON.parse(e.data);
-    //   setMessages((prevMessages) => [...prevMessages, data]);
-    // };
-
-    // ws.onerror = (error) => {
-    //   console.error('WebSocket error:', error);
-    // };
-
-    // ws.onclose = () => {
-    //   console.log('WebSocket closed');
-    // };
-
-    // // Set the WebSocket connection in state
-    // setSocket(ws);
-
-    // return () => {
-    //   // Close the WebSocket connection when the component unmounts
-    //   if (socket) {
-    //     socket.close();
-    //   }
-    // };
-    
-  }, [backendUrl, slug, socket]);
+  const sendMessage = async () => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(JSON.stringify({ message: newMessage, user: userData.id }));
+      setNewMessage('');
+    }
+  };
 
   return (
-    <div className="flex-1 overflow-auto bg-purple-100">
-      <div className="py-2 px-3">
-        {/* Dynamic Message Rendering */}
-        {isLoading ? (
-          <div>Loading messages...</div>
-        ) : (
-          messages.map((message, index) => {
-            const isCurrentUserMessage = message.user === userData.id;
-            return (
-              <div key={index} className={`flex ${isCurrentUserMessage ? 'justify-end' : ''} mb-2 items-end`}>
-                {!isCurrentUserMessage && (
-                  // User ID button for messages from other users
-                  <div className="rounded-full bg-blue-500 text-white text-xs mr-2 px-3 py-1">
-                    {message.user}
-                  </div>
-                )}
-                <div className={`rounded py-2 px-3 ${isCurrentUserMessage ? 'bg-green-300' : 'bg-slate-300'}`}>
-                  <p className="text-sm mt-1">{message.content}</p>
-                  <p className="text-right text-xs text-grey-dark mt-1">{new Date(message.timestamp).toLocaleTimeString()}</p>
-                </div>
-                {isCurrentUserMessage && (
-                  // User ID button for current user's messages
-                  <div className="rounded-full bg-red-500 text-white text-xs ml-2 px-3 py-1">
-                    {message.user}
-                  </div>
-                )}
-              </div>
-            );
-          })
-        )}
-
-    <div className="mt-4">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="Type a message..."
-            className="px-3 py-2 border rounded-lg w-full"
-          />
-          <button onClick={sendMessage} className="bg-blue-500 text-white px-3 py-2 rounded-lg mt-2">
-            Send
-          </button>
-        </div>
+    <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '600px', margin: '0 auto' }}>
+      <div style={{ height: '400px', overflowY: 'scroll', padding: '10px', backgroundColor: '#E5DDD5', border: '1px solid #ddd' }}>
+        {isLoading ? <div>Loading messages...</div> : messages.map((msg, index) => (
+          <div key={index} style={{
+            textAlign: msg.user === userData.id ? 'right' : 'left',
+            marginBottom: '10px'
+          }}>
+            <div style={{
+              display: 'inline-block',
+              maxWidth: '80%',
+              backgroundColor: msg.user === userData.id ? '#DCF8C6' : 'white',
+              padding: '8px 10px',
+              borderRadius: '10px',
+              boxShadow: '0 1px 1px rgba(0, 0, 0, 0.1)'
+            }}>
+              <p style={{ margin: 0, fontWeight: 'bold' }}>{msg.user}</p>
+              <p style={{ margin: 0 }}>{msg.content}</p> {/* Displaying the message content */}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div style={{ display: 'flex', padding: '10px', borderTop: '1px solid #ddd' }}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type a message..."
+          style={{ flex: 1, padding: '10px', border: '1px solid #ddd', borderRadius: '20px', marginRight: '10px' }}
+        />
+        <button onClick={sendMessage} style={{ backgroundColor: '#075E54', color: 'white', padding: '10px 15px', border: 'none', borderRadius: '20px' }}>
+          Send
+        </button>
       </div>
     </div>
-      
   );
 };
 
