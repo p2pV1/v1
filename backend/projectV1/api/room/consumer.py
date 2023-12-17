f31f244f70e869a2fc32342998ab8f268c7623c4
Ashish Regmi <ashishregmi2017@gmail.com>
2023-12-17 15:40:20 -0500

    Consumer.py updated

--- backend/projectV1/api/room/consumer.py
+++ backend/projectV1/api/room/consumer.py
@@ -1,39 +1,51 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from room.models import Message, ChatRoom
+from django.utils import timezone
 import json
-
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['slug']
         self.room_group_name = f'chat_{self.room_name}'
-
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
+        message_text = text_data_json['message']
+        # Save message to the database
+        message = await self.save_message(message_text)
+        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'content': message_text,
+                'user_id': message.user.id,
+                'timestamp': str(message.timestamp)
             }
         )
-
+    @database_sync_to_async
+    def save_message(self, message_text):
+        # Assuming self.scope["user"] is the authenticated user
+        room = ChatRoom.objects.get(slug=self.room_name)
+        return Message.objects.create(
+            room=room,
+            user=self.scope['user'],
+            content=message_text,
+            timestamp=timezone.now()
+        )
     async def chat_message(self, event):
-        message = event['message']
-
+        # Send a message down to the client
         await self.send(text_data=json.dumps({
-            'message': message
+            'content': event['message'],
+            'user_id': event['user_id'],
+            'timestamp': event['timestamp']
         }))
