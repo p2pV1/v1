9d8e384b7e5e043b1aad656618780bb9e72e02c0
Pratik Dahal <pratikdahal105@gmail.com>
2023-12-17 13:09:07 -0500

    redis server

--- backend/projectV1/projectV1/asgi.py
+++ backend/projectV1/projectV1/asgi.py
@@ -2,7 +2,17 @@
 
 import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from api.room.routing import websocket_urlpatterns  # Replace 'my_app' with your app name
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectV1.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
--- backend/projectV1/room/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-
-from django.urls import re_path
-from room import consumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<slug>\w+)/$', consumer.ChatConsumer.as_asgi()),
-]
