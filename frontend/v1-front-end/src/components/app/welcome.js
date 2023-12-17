466dcc792f8a632f5a7beb12ada0c12a0b857076
chiran gurung <grgchiran08@yahoo.com>
2023-12-17 16:07:04 -0500

    Frontend-Backend Merge

--- frontend/v1-front-end/src/components/app/welcome.js
+++ frontend/v1-front-end/src/components/app/welcome.js
@@ -1,22 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Categories } from "./categories";
 
 export function Welcome({ userData }) {
   return (
-    <main className="content">
-      <h3>Get Started with picking a few interests</h3>
-
+    <main className="flex flex-col items-center bg-gradient min-h-screen">
       {userData ? (
-        <div>
-          <p>Welcome, {userData.username}!</p>
-          <p>Email: {userData.email}</p>
-          <p>ID: {userData.id}</p>
-          {/* Display other user data as needed */}
+        <div className="mt-20">
+          <div className="max-w-sm w-64 mx-auto bg-white  shadow-xl hover:shadow flex items-center justify-center rounded-lg">
+            <div className="px-6 py-4 text-center">
+              <div className="mb-4 border-b">
+                {/* Profile Image */}
+                <img
+                  className="w-24 h-24 mx-auto rounded-full -mt-20 border-8 border-white"
+                  src="https://avatars.githubusercontent.com/u/67946056?v=4"
+                  alt="Profile"
+                />
+              </div>
+              {/* Profile Name */}
+              <div className="font-bold text-xl mb-2">{userData.id}</div>
+              {/* Username */}
+              <p className="text-gray-700 text-base">{userData.username}!</p>
+              {/* Location */}
+              <p className="text-gray-600 text-sm">{userData.email}</p>
+            </div>
+          </div>
         </div>
       ) : (
         <p>User data not available.</p>
       )}
-      <Categories />
+
+      <div className="flex justify-center flex-col sm:flex-row ">
+        <Link
+          to="/create-room"
+          className="bg-gray-200 h-56 w-56 object-cover mx-auto my-4 rounded-lg md:mx-4 md:my-3 flex items-center justify-center font-bold text-2xl text-purple-700 hover:text-white hover:bg-purple-600 transition duration-150 ease-in-out shadow-md"
+        >
+          Rooms
+        </Link>
+        <div className="bg-gray-200 h-56 w-56 object-cover mx-auto my-4 rounded-lg md:mx-4 md:my-3 flex items-center justify-center font-bold text-2xl text-purple-700 hover:text-white hover:bg-purple-600 transition duration-150 ease-in-out shadow-md">
+          Coming Soon...
+        </div>
+        {/* <Categories /> */}
+      </div>
     </main>
   );
 }
