4c548ccc8385f2e9f6ea52a5e842103140aba2d9
Ashish Regmi <ashishregmi2017@gmail.com>
2023-12-28 20:32:20 -0500

    Removed Unnecessary Comments

--- frontend/v1-front-end/src/components/chat/roomdetails.js
+++ frontend/v1-front-end/src/components/chat/roomdetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MessageArea from "./messagearea";
+import { useSelector } from "react-redux";
 
 const RoomDetails = () => {
   const { backendUrl } = useSelector((state) => state.backendUrl);
